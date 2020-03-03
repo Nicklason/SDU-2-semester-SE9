df42fcd3807c143e664b39c826232095107ed764
Kasper <Kasperjalris@hotmail.com>
2020-03-03 18:53:00 +0100

    I set the Title to "management System"
    Testing intelliJ with our project

--- src/main/java/sdu/se9/tv2/management/system/App.java
+++ src/main/java/sdu/se9/tv2/management/system/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TitledPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +20,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
+        stage.setTitle("Management System");
         stage.show();
     }
 
