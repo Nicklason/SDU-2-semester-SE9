4a94638cc33804235e5c585c88d00b4a2fee84a9
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-04-15 14:35:13 +0200

    add system constructor

--- src/main/java/sdu/se9/tv2/management/system/ManagementSystem.java
+++ src/main/java/sdu/se9/tv2/management/system/ManagementSystem.java
@@ -1,7 +1,6 @@
 package sdu.se9.tv2.management.system;
 
 public class ManagementSystem {
-
     private static ManagementSystem instance;
 
     public static ManagementSystem getSingleton() {
@@ -10,6 +9,21 @@ public class ManagementSystem {
         }
 
         return instance;
+    }
+
+    private PersistenceProducer persistenceProducer;
+
+    private PersistenceProgram persistenceProgram;
+
+    private PersistencePerson persistencePerson;
+
+    private PersistenceCredit persistenceCredit;
+
+    ManagementSystem () {
+        this.persistenceProducer = new PersistenceProducer();
+        this.persistenceProgram = new PersistenceProgram();
+        this.persistencePerson = new PersistencePerson();
+        this.persistenceCredit = new PersistenceCredit();
     }
 
     public Credit addCredits (int programID, int personID, String roleName){
