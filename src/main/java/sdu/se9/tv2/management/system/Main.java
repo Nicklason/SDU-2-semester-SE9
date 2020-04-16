c24bf3560a039d98ab022676f33b7e3d3971625c
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-04-16 14:05:18 +0200

    remove unused imports

--- src/main/java/sdu/se9/tv2/management/system/Main.java
+++ src/main/java/sdu/se9/tv2/management/system/Main.java
@@ -1,9 +1,5 @@
 package sdu.se9.tv2.management.system;
 
-import sdu.se9.tv2.management.system.exceptions.UsernameAlreadyExistsException;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main (String args[]) {
         ManagementSystem system = new ManagementSystem();
