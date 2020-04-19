270c6265400290df04eb7633a5c699176fe3a9b3
Kingofo <magnuskingos@gmail.com>
2020-04-19 20:05:21 +0200

    Login DONE

--- src/main/java/sdu/se9/tv2/management/system/presentation/LogInLogOutController.java
+++ src/main/java/sdu/se9/tv2/management/system/presentation/LogInLogOutController.java
@@ -2,22 +2,26 @@ package sdu.se9.tv2.management.system.presentation;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import sdu.se9.tv2.management.system.domain.accounts.Account;
-import sdu.se9.tv2.management.system.persistence.IPersistenceAccount;
+import sdu.se9.tv2.management.system.persistence.PersistenceAccount;
+
+import java.io.IOException;
 
 public class LogInLogOutController {
     private Account account;
-    private IPersistenceAccount persistenceAccount;
 
     @FXML
     private TextField usernameInput;
     @FXML
     private PasswordField passwordInput;
+    @FXML
+    private Label loginError;
 
     @FXML
-    public void login(ActionEvent event) {
+    public void login(ActionEvent event) throws IOException {
         System.out.println("Hvad er dit brugernavn?");
 
         String username = usernameInput.getText();
@@ -30,18 +34,15 @@ public class LogInLogOutController {
 
         System.out.println(password);
 
-        Account account = IPersistenceAccount.getMatchingAccount(username, password);
+        Account account = PersistenceAccount.getInstance().getMatchingAccount(username, password);
         if (account == null) {
-            System.out.println("Forkert brugernavn/adgangskode");
+            loginError.setText("Brugernavn eller adgangskoder er forkert");
             return;
         }
 
         this.account = account;
         System.out.println(account);
+        App.setPage("homepage");
     }
 
-    public void logout () {
-        this.account = null;
-        System.out.println("Du er nu logget ud");
-    }
 }
