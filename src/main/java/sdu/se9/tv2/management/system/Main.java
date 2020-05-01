825b41680e94e975aa304af00000101f79cd9e64
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-05-01 14:38:04 +0200

    add config for database connection

--- src/main/java/sdu/se9/tv2/management/system/Main.java
+++ src/main/java/sdu/se9/tv2/management/system/Main.java
@@ -1,9 +1,14 @@
 package sdu.se9.tv2.management.system;
 
+import sdu.se9.tv2.management.system.helpers.Config;
 import sdu.se9.tv2.management.system.presentation.App;
 
 public class Main {
     public static void main (String args[]) {
+        // Load config
+        Config.load();
+
+        // Start JavaFX
         App.load();
     }
 }
--- src/main/java/sdu/se9/tv2/management/system/persistence/PersistenceDatabaseHelper.java
+++ src/main/java/sdu/se9/tv2/management/system/persistence/PersistenceDatabaseHelper.java
@@ -1,5 +1,7 @@
 package sdu.se9.tv2.management.system.persistence;
 
+import sdu.se9.tv2.management.system.helpers.Config;
+
 import java.sql.*;
 
 public class PersistenceDatabaseHelper {
@@ -10,14 +12,32 @@ public class PersistenceDatabaseHelper {
             try {
                 DriverManager.registerDriver(new org.postgresql.Driver());
 
-                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tv2",
-                        "postgres",
-                        "1234");
+                connection = DriverManager.getConnection((String)Config.get("database_connection_url"),
+                        (String)Config.get("database_user"),
+                        (String)Config.get("database_password"));
             } catch (SQLException err) {
                 err.printStackTrace();
             }
         }
 
         return connection;
+    }
+
+    public static void main(String[] args) {
+        // Load config
+        Config.load();
+
+        // Get connection
+        Connection connection = getConnection();
+
+        // Make query
+        try {
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM SomeTable");
+            ResultSet result = stmt.executeQuery();
+
+            System.out.println(result);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
