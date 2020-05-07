ec7344ae84cac6bfe80f62d5b3809bb8e79f0565
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-05-07 14:58:53 +0200

    fix indentations

--- src/main/java/sdu/se9/tv2/management/system/persistence/PersistenceCredit.java
+++ src/main/java/sdu/se9/tv2/management/system/persistence/PersistenceCredit.java
@@ -27,42 +27,40 @@ public class PersistenceCredit implements IPersistenceCredit {
 
     @Override
     public Credit createCredit(int programID, int personID, String roleName) throws DuplicateRoleNameException, SQLException {
-
         Connection connection = PersistenceDatabaseHelper.getConnection();
 
-            if (this.getCredit(programID, roleName) != null) {
-                throw new DuplicateRoleNameException("Role already exists");
-            }
+        if (this.getCredit(programID, roleName) != null) {
+            throw new DuplicateRoleNameException("Role already exists");
+        }
 
-            PreparedStatement stmt = connection.prepareStatement("INSERT INTO Credit(programID, personID, roleName) VALUES(?,?,?) RETURNING id;");
-            stmt.setInt(1,  programID);
-            stmt.setInt(2,  personID);
-            stmt.setString(3,  roleName);
+        PreparedStatement stmt = connection.prepareStatement("INSERT INTO Credit(programID, personID, roleName) VALUES(?,?,?) RETURNING id;");
+        stmt.setInt(1,  programID);
+        stmt.setInt(2,  personID);
+        stmt.setString(3,  roleName);
 
-            ResultSet rs = stmt.executeQuery();
-            rs.next();
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
 
-            int id = rs.getInt(1);
+        int id = rs.getInt(1);
 
-            return new Credit(id, programID, personID, roleName);
+        return new Credit(id, programID, personID, roleName);
     }
 
     @Override
     public ArrayList<Credit> getCredits(int programID) throws SQLException {
         Connection connection = PersistenceDatabaseHelper.getConnection();
 
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Credit WHERE programID = ?;");
+        stmt.setInt(1, programID);
 
-            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Credit WHERE programID = ?;");
-            stmt.setInt(1, programID);
+        ResultSet result = stmt.executeQuery();
 
-            ResultSet result = stmt.executeQuery();
+        ArrayList<Credit> returnValue = new ArrayList<>();
+        while (result.next()){
+            returnValue.add(new Credit(result.getInt("id"), result.getInt("programID"), result.getInt("personID"), result.getString("roleName")));
+        }
 
-            ArrayList<Credit> returnValue = new ArrayList<>();
-            while (result.next()){
-                returnValue.add(new Credit(result.getInt("id"), result.getInt("programID"), result.getInt("personID"), result.getString("roleName")));
-            }
-
-            return returnValue;
+        return returnValue;
     }
 
     /**
@@ -74,63 +72,61 @@ public class PersistenceCredit implements IPersistenceCredit {
     public ArrayList<Credit> getCredits(int programID, int personID) throws SQLException {
         Connection connection = PersistenceDatabaseHelper.getConnection();
 
-            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Credit WHERE programID = ? AND personID = ?;");
-            stmt.setInt(1, programID);
-            stmt.setInt(2, personID);
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Credit WHERE programID = ? AND personID = ?;");
+        stmt.setInt(1, programID);
+        stmt.setInt(2, personID);
 
-            ResultSet result = stmt.executeQuery();
+        ResultSet result = stmt.executeQuery();
 
-            ArrayList<Credit> returnValue = new ArrayList<>();
-            while (result.next()){
-                returnValue.add(new Credit(result.getInt("id"), result.getInt("programID"), result.getInt("personID"), result.getString("roleName")));
-            }
+        ArrayList<Credit> returnValue = new ArrayList<>();
+        while (result.next()){
+            returnValue.add(new Credit(result.getInt("id"), result.getInt("programID"), result.getInt("personID"), result.getString("roleName")));
+        }
 
-            System.out.println(result);
-            return returnValue;
+        System.out.println(result);
+        return returnValue;
     }
 
     public Credit getCredit (int programID, int personID, String roleName) throws SQLException {
+        PreparedStatement stmt = PersistenceDatabaseHelper.getConnection().prepareStatement("SELECT * FROM Credit WHERE programID = ? AND personID = ?" +
+                " AND roleName = ?;");
+        stmt.setInt(1, programID);
+        stmt.setInt(2, personID);
+        stmt.setString(3, roleName);
 
-            PreparedStatement stmt = PersistenceDatabaseHelper.getConnection().prepareStatement("SELECT * FROM Credit WHERE programID = ? AND personID = ?" +
-                    " AND roleName = ?;");
-            stmt.setInt(1, programID);
-            stmt.setInt(2, personID);
-            stmt.setString(3, roleName);
+        ResultSet rs = stmt.executeQuery();
 
-            ResultSet rs = stmt.executeQuery();
+        if (!rs.next()) {
+            // No match
+            return null;
+        }
 
-            if (!rs.next()) {
-                // No match
-                return null;
-            }
+        int id = rs.getInt("id");
+        int program = rs.getInt("programID");
+        int person = rs.getInt("personID");
+        String role = rs.getString("roleName");
 
-            int id = rs.getInt("id");
-            int program = rs.getInt("programID");
-            int person = rs.getInt("personID");
-            String role = rs.getString("roleName");
-
-            return new Credit(id, program, person, role);
+        return new Credit(id, program, person, role);
     }
 
     public Credit getCredit (int programID, String roleName) throws SQLException {
+        PreparedStatement stmt = PersistenceDatabaseHelper.getConnection().prepareStatement("SELECT * FROM Credit WHERE programID = ? AND roleName = ?;");
+        stmt.setInt(1, programID);
+        stmt.setString(2, roleName);
 
-            PreparedStatement stmt = PersistenceDatabaseHelper.getConnection().prepareStatement("SELECT * FROM Credit WHERE programID = ? AND roleName = ?;");
-            stmt.setInt(1, programID);
-            stmt.setString(2, roleName);
+        ResultSet rs = stmt.executeQuery();
 
-            ResultSet rs = stmt.executeQuery();
+        if (!rs.next()) {
+            // No match
+            return null;
+        }
 
-            if (!rs.next()) {
-                // No match
-                return null;
-            }
+        int id = rs.getInt("id");
+        int program = rs.getInt("programID");
+        int person = rs.getInt("personID");
+        String role = rs.getString("roleName");
 
-            int id = rs.getInt("id");
-            int program = rs.getInt("programID");
-            int person = rs.getInt("personID");
-            String role = rs.getString("roleName");
-
-            return new Credit(id, program, person, role);
+        return new Credit(id, program, person, role);
     }
 
 
@@ -141,21 +137,20 @@ public class PersistenceCredit implements IPersistenceCredit {
     public ArrayList<Credit> getCreditsByPerson (int personID, int maxCount) throws SQLException {
         Connection connection = PersistenceDatabaseHelper.getConnection();
 
-            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Credit WHERE personID = ? LIMIT ?;");
-            stmt.setInt(1, personID);
-            stmt.setInt(2, maxCount);
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Credit WHERE personID = ? LIMIT ?;");
+        stmt.setInt(1, personID);
+        stmt.setInt(2, maxCount);
 
-            ResultSet result = stmt.executeQuery();
+        ResultSet result = stmt.executeQuery();
 
-            ArrayList<Credit> returnValue = new ArrayList<>();
-            while (result.next()){
-                returnValue.add(new Credit(result.getInt("id"), result.getInt("programID"), result.getInt("personID"), result.getString("roleName")));
-                if (returnValue.size() >= maxCount) {
-                    break;
-                }
+        ArrayList<Credit> returnValue = new ArrayList<>();
+        while (result.next()){
+            returnValue.add(new Credit(result.getInt("id"), result.getInt("programID"), result.getInt("personID"), result.getString("roleName")));
+            if (returnValue.size() >= maxCount) {
+                break;
             }
+        }
 
-            return returnValue;
+        return returnValue;
     }
 }
-
