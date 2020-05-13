a1458824d0bef42a370bb061ad9c61a563ccdd1a
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-05-13 17:47:33 +0200

    make MainController use interface instead of class

--- src/main/java/sdu/se9/tv2/management/system/presentation/MainController.java
+++ src/main/java/sdu/se9/tv2/management/system/presentation/MainController.java
@@ -2,11 +2,14 @@ package sdu.se9.tv2.management.system.presentation;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import sdu.se9.tv2.management.system.domain.IManagementSystem;
 import sdu.se9.tv2.management.system.domain.ManagementSystem;
 
 import java.io.IOException;
 
 public class MainController {
+    IManagementSystem managementSystem = ManagementSystem.getInstance();
+
     @FXML
     private void handleHomepageShowView(ActionEvent e) throws IOException {
         App.setPage("homepage");
@@ -19,10 +22,8 @@ public class MainController {
 
     @FXML
     private void handleLoginShowView(ActionEvent e) throws IOException {
-        ManagementSystem system = ManagementSystem.getInstance();
-
-        if(system.isLoggedIn()){
-            system.setAccount(null);
+        if(managementSystem.isLoggedIn()) {
+            managementSystem.setAccount(null);
             App.updateLogin();
         } else{
             App.setPage("login");
